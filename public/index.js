ac3e3f1bcb6927c435931ff592f00ede8d5af909
Cait Scherr <scherr.ca@gmail.com>
2015-07-25 15:19:20 -0700

    changed image links

--- public/index.js
+++ public/index.js
@@ -27,15 +27,15 @@ function createImage() {
 }
 
 allImages = [
-{"src": "../res/images/cat-stirfry.jpg"},
-{"src": "../res/images/cub-playing.png", "orientation":"landscape", "mood":"cute"},
-{"src": "../res/images/curious-raccoons.jpg", "orientation":"landscape", "mood":"cute"},
-{"src": "../res/images/tiger-boop.png", "orientation":"landscape", "mood":"cute"},
-{"src": "../res/images/tiger-cub.jpg", "orientation":"landscape", "mood":"cute"},
-{"src": "../res/images/dog.png", "orientation":"landscape", "mood":"sad"},
-{"src": "../res/images/kitten.png", "orientation":"landscape", "mood":"happy"},
-{"src": "../res/images/koala.png", "orientation":"landscape", "mood":"inclusivity"},
-{"src": "../res/images/monkey.png", "orientation":"landscape", "mood":"cute"}
+{"src": "images/cat-stirfry.jpg"},
+{"src": "images/cub-playing.png", "orientation":"landscape", "mood":"cute"},
+{"src": "images/curious-raccoons.jpg", "orientation":"landscape", "mood":"cute"},
+{"src": "images/tiger-boop.png", "orientation":"landscape", "mood":"cute"},
+{"src": "images/tiger-cub.jpg", "orientation":"landscape", "mood":"cute"},
+{"src": "images/dog.png", "orientation":"portrait", "mood":"sad"},
+{"src": "images/kitten.png", "orientation":"landscape", "mood":"happy"},
+{"src": "images/koala.png", "orientation":"landscape", "mood":"inclusivity"},
+{"src": "images/monkey.png", "orientation":"landscape", "mood":"cute"}
 ];
 
 maxIndex = allImages.length - 1;
